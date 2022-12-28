28f7eb51d812d0cd734873f4b927546eb71fd6e1
Grissela <108595899+Grissela@users.noreply.github.com>
2022-12-28 17:26:51 -0500

    Proyecto completado

--- src/app/modulos/modulos.module.ts
+++ src/app/modulos/modulos.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PageComponent } from './page/page.component';
+// import { PageComponent } from './page/page.component';
 
 
 
 @NgModule({
   declarations: [
-    PageComponent
+    // PageComponent
   ],
   imports: [
     CommonModule
